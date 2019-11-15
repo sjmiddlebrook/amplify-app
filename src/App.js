9839f8663a3e491d6351378f4ca78f698ce2f6a2
Jack Middlebrook <jack.middlebrook@gmail.com>
2019-11-15 23:31:58 +0100

    Remove authenticator on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react'
-import { withAuthenticator } from 'aws-amplify-react'
 
 function App() {
     return (
         <div>
-            <h1>Hello from AWS Amplify</h1>
+            <h1>Hello AWS Amplify</h1>
         </div>
     )
 }
 
-export default withAuthenticator(App, { includeGreetings: true })
+export default App;
